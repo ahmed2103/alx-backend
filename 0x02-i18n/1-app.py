9508802b5f187d5c7c6dc10631399aef11736736
Hasona <bekoahmed20@gmail.com>
2024-07-30 23:53:20 +0300

    all

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
-"""simple flask module"with simple route"""
+"""A simple Flask app."""
 
-from flask import Flask, render_template, request
 from flask_babel import Babel
+from flask import Flask, render_template
 
 
 class Config:
-    """configuration class"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """Represents a Flask Babel configuration."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
-
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
 
-@app.route("/")
-def index():
-    """render index html"""
-    return render_template('0-index.html')
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
-"""simple flask module"with simple route"""
+"""A simple Flask app."""
 
+from flask_babel import Babel
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
 
 
 class Config:
-    """configuration class"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """Represents a Flask Babel configuration."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
-
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """Determines the locale of the current request"""
-    return request.accept_languages.best_match(app.config.LANGUAGES)
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route("/")
-def index():
-    """render index html"""
-    return render_template('3-index.html', home_title=_("home_title"),
-                           home_header=_("home_header"))
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('3-index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
